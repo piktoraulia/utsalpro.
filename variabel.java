01f8cc4a945601709d70dc96d5ed80b33e2aa3de
piktoraulia <piktoraulia3@gmail.com>
2025-11-27 18:48:24 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,21 @@
+public class variabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data yang sesuai
+        String nama = " Piktor aulia";
+        String tempatTanggalLahir = "Bogor, 18 maret 2005";
+        String jenisKelamin = "Laki-laki";
+        String alamatLengkap = "Kp pasir gaok, Bogor";
+        String noHp = "085714748408";
+        Double penghasilanPerbulan = 2500000.0;
+
+        System.out.println("Assalamualaikum Wr. Wb.");
+        System.out.println("Izinkan saya memperkenalkan diri:");
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Tempat, Tanggal Lahir: " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Alamat Lengkap: " + alamatLengkap);
+        System.out.println("No. HP: " + noHp);
+        System.out.println("Penghasilan Perbulan: Rp " + penghasilanPerbulan);
+    }
+}
